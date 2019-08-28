df995a8e50f97a91e7b6610f69a1e9ac3d51d007
grin-quokka <grin_quokka@gmail.com>
2019-08-28 21:20:30 +0900

    fetch movie list data

---
+++ build/precache-manifest.62dc106149e774780a243652a4fb1e24.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a2dfbce288a5eccc9f8bf4d6518bdab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffa0eb65fd097de00550",
+    "url": "/static/js/2.b4040e45.chunk.js"
+  },
+  {
+    "revision": "20b8306c2501ebe0e253",
+    "url": "/static/js/main.9a93d0c1.chunk.js"
+  },
+  {
+    "revision": "b5cb37b41c87a9ab5e0b",
+    "url": "/static/js/runtime~main.9ce94da8.js"
+  }
+]);
